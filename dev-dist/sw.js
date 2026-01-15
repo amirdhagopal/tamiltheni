d0ac1c34ca3c05323a08aa1e9170c1455df10c83
Amirdha Gopal R <amirdhagopal@gmail.com>
2026-01-14 18:00:43 -0600

    Update dev service worker

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-2ae722a1'], (function (workbox) { 'use strict';
     "revision": "ea8837cc7ab1e9d3bcd9ba14f72f26bf"
   }, {
     "url": "index.html",
-    "revision": "0.i32661cmkms"
+    "revision": "0.1mr1uq85k2k"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
